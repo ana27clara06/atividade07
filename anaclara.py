219cba7f3b509accab5888821c76db09b6956b2a
ana27clara06 <4n4clara@gmail.com>
2023-10-18 11:26:50 -0300

    first commit

---
+++ anaclara.py
@@ -0,0 +1,6 @@
+real = float(input("digite o valor em reais"))
+dolar = float(input("digite a cotação atual"))
+conversão = real/dolar
+print("%.2f"%conversão)
+print(f"seu valor em dolar é{conversão}.3f).3f")
+
